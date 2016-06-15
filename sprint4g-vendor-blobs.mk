63614c6b6bb8a20e008ee475f85821ced224e0b8
Ubuntu <darklord4822@SRT.SRT-50t7o38d.b8.internal.cloudapp.net>
2016-06-15 21:58:29 +0000

    first commit

---
+++ sprint4g-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/smart/sprint4g/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/smart/sprint4g/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/smart/sprint4g/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/smart/sprint4g/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/smart/sprint4g/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/smart/sprint4g/vendor,system/vendor)
